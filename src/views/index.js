85ed10f1a2f97f64bcc8d1fb2633b3809b2e32ed
reaper <ahoy@barelyhuman.dev>
2024-11-11 12:37:56 +0530

    fix: link layer overlap[

--- src/views/index.js
+++ src/views/index.js
@@ -2,12 +2,7 @@ import '../global.css'
 import BaseLayout from '../layouts/base.jsx'
 import data from '../data/links.json'
 
-/**
- * @param {object} options
- * @param {import("fastify").FastifyRequest} options.request
- * @returns
- */
-export default function Page({}) {
+export default function IndexView() {
   return (
     <BaseLayout>
       <div class="min-h-[100vh] mx-auto max-w-lg">
@@ -27,16 +22,46 @@ export default function Page({}) {
 }
 
 function RenderLink({ link }) {
+  const description = constructDescription(link)
   return (
     <li class="py-3 my-3 segment-dividers">
       <a
         href={link.link}
         class="inline-flex gap-1 items-center hover:text-lime"
         target="_blank"
+        rel="noopener noreferrer"
       >
         {link.name} <span class="text-xs iconify ph--arrow-up-right" />
       </a>
-      <p class="text-zinc-600">{link.description}</p>
+      <p class="text-zinc-600">{description}</p>
     </li>
   )
+}
+
+function constructDescription(linkSet) {
+  let resultString = linkSet.description
+  if (!linkSet?.replacements) {
+    return resultString
+  }
+  Object.keys(linkSet.replacements).forEach(k => {
+    const replacement = linkSet.replacements[k]
+    if (replacement.type === 'link') {
+      const before = resultString.slice(0, replacement.at.start)
+      const after = resultString.slice(replacement.at.end)
+      const label = resultString.slice(replacement.at.start, replacement.at.end)
+      resultString = [
+        before,
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          class="underline text-zinc-500 hover:text-lime underline-offset-4"
+          href={replacement.value}
+        >
+          {label}
+        </a>,
+        after,
+      ]
+    }
+  })
+  return resultString
 }
